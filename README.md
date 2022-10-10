8d0c9bf6d49c945d51379ecfbf625492d1d54322
Дмитрий Куулар <kuulardmo@bk.ru>
2022-10-10 22:01:27 +0700

    Created settings

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[![Build status](https://ci.appveyor.com/api/projects/status/m1wf92ww005qa3vp?svg=true)](https://ci.appveyor.com/project/DmitriiKuular/gradlehwthird)
---
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -0,0 +1,34 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+//        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearsDown() {
+        driver.quit();
+        driver=null;
+    }
+}
